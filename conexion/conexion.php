ba7307a3555bd3c75f106215a1eed8bece8aa443
emanuel <emanueldavidroca@gmail.com>
2022-07-02 14:41:16 -0300

    zulhow db

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -4,10 +4,10 @@
 	{
 		public static function conectar()
 		{
-			$dbname = "zulhow";
-			$userdb = "root";
-			$passdb = "";
-			$ip = "localhost";
+			$dbname = "zulhow_db";
+			$userdb = "zulhow";
+			$passdb = "159aSd753@";
+			$ip = "mysql-zulhow.alwaysdata.net";
 
 			$link = new PDO ("mysql:host=".$ip.";dbname=".$dbname."",$userdb,$passdb,array(PDO::MYSQL_ATTR_FOUND_ROWS => true));
 			$link->exec("SET CHARACTER SET utf8");
