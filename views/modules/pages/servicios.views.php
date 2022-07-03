2b81c063c2df1be74272b0b492050364d5640bd6
emanuel <emanueldavidroca@gmail.com>
2022-07-03 18:54:53 -0300

    img

--- views/modules/pages/servicios.views.php
+++ views/modules/pages/servicios.views.php
@@ -23,7 +23,7 @@
       </div> 
 
       <picture>
-          <source media="(max-width: 967px)" srcset="/views/assets/img/portada.jpg">
+          <source media="(max-width: 967px)" srcset="/views/assets/img/portada1.jpg">
            <img class="img-fluid"  src="/views/assets/img/portada.jpg" alt="Imagen de portada de camiones con el logo de zulhow s.a"/>
        </picture>              
                
