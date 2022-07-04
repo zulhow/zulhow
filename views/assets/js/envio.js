b4ede24ad3233f048de4eaf20feb59b21a882c46
emanuel <emanueldavidroca@gmail.com>
2022-07-03 21:47:33 -0300

    js

--- views/assets/js/envio.js
+++ views/assets/js/envio.js
@@ -116,8 +116,8 @@ function form_envios(element){
 		error = true;
 		destino_localidad.parentNode.querySelector(".error").classList.add("active");
 	}
-	console.log(franja_inicio.parentNode.querySelector(".error"));
 	if (franja_inicio_value.value == -1) {
+		console.log(franja_inicio.parentNode.querySelector(".error"));
 		error = true;
 		franja_inicio.parentNode.querySelector(".error").classList.add("active");
 	}
